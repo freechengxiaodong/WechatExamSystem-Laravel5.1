3fde6c37b4468c488c6a89571783d35469184d7e
songchuanwei <1936498903@qq.com>
2018-09-08 16:57:26 +0800

    song

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,8 +21,8 @@ Route::group(['middleware' => ['wechat.oauth']], function () {
 
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
-Route::controller('index','Admin/IndexController');//后台首页
-Route::controller('student','Admin/StudentsController');//后台学生管理
-Route::controller('teacher','Admin/TeachersController');//后台老师管理
-Route::controller('test','Admin/TestsController');//后台题库管理	
+//Route::controller('index','Admin/IndexController');//后台首页
+//Route::controller('student','Admin/StudentsController');//后台学生管理
+//Route::controller('teacher','Admin/TeachersController');//后台老师管理
+//Route::controller('test','Admin/TestsController');//后台题库管理	
 
