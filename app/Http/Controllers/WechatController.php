d3e86a4f1b1a0eef7e7990bcf6bb67bfa6c05b24
freecheng <1678029425@qq.com>
2018-09-09 08:49:27 +0800

    cxd

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -15,7 +15,7 @@ class WechatController extends Controller
         //1.将timestamp,nonce,token按字典序排列
         $timestamp= $_GET['timestamp'];
         $nonce = $_GET['nonce'];
-        $token  = 'religion';   //宗教
+        $token  = 'wechat';   //宗教
         $signature = $_GET['signature'];
         $echostr = $_GET['echostr'];
 
