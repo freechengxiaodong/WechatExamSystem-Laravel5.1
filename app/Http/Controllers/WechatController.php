475e127538ef83a6090f490089537286f4b70c1e
freecheng <1678029425@qq.com>
2018-09-09 08:02:47 +0800

    cxd

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Log;
-use EasyWeChat\Factory;
+use EasyWeChat;
 
 class WechatController extends Controller
 {
