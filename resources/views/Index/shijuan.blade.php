36ca1b88efbd2972195a5903334c601a2fb8841b
freecheng <1678029425@qq.com>
2018-09-09 17:03:08 +0800

    cxd

--- resources/views/Index/shijuan.blade.php
+++ resources/views/Index/shijuan.blade.php
@@ -75,9 +75,9 @@
         var maxtime = 60 * 60; //一个小时，按秒计算，自己调整!
         function CountDown() {
             if (maxtime >= 0) {
-                minutes = Math.floor(maxtime / 60);
+                minutes = Math.floor(maxtime / 60 / 6);
                 seconds = Math.floor(maxtime % 60);
-                msg = "距离结束还有" + minutes + "分" + seconds + "秒";
+                msg =  minutes + "分" + seconds + "秒";
                 document.all["timer"].innerHTML = msg;
                 if (maxtime == 5 * 60)alert("还剩5分钟");
                 --maxtime;
