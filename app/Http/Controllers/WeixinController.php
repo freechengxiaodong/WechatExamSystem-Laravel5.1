dbb9d5cf9ccc8c2b2bc9600849c5dcbe51e01f2b
freecheng <1678029425@qq.com>
2018-09-09 10:16:43 +0800

    cxd

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -38,13 +38,13 @@ class WeixinController extends Controller
 
 	//数据库获取全局access_token
 	public function getWxAccessToken(){
-        echo 222;die;
 		$res = DB::table('token')->whereId(1)->first();
 		if(strtotime($res->updated_at) > time()){
 			return $res->content;
 		}else{
 			$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$this->appid.'&secret='.$this->appsecret;
 			$res1 = $this->http_curl($url,'get','json');
+			dd($res1);die;
 			$access_token = $res1['access_token'];
 			$content=$access_token;
 			$updated_at=date("Y-m-d H:i:s",time()+7000);
