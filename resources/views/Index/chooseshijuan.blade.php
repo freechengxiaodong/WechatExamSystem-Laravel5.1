5f259c3d149c017ac7be8060c7359fc4d1072395
freecheng <1678029425@qq.com>
2018-09-10 09:53:15 +0800

    cxd

--- resources/views/Index/chooseshijuan.blade.php
+++ resources/views/Index/chooseshijuan.blade.php
@@ -6,6 +6,90 @@
             margin: 0 auto;
         }
     </style>
+    <div class="weui-tab">
+        <div class="weui-navbar">
+            <div class="weui-navbar__item weui-bar__item--on">
+                选项一
+            </div>
+            <div class="weui-navbar__item">
+                选项二
+            </div>
+            <div class="weui-navbar__item">
+                选项三
+            </div>
+        </div>
+        <div class="weui-tab__bd">
+
+            <input type="text" id="a">
+
+        </div>
+
+        <div class="weui-tabbar">
+            <a href="javascript:;" class="weui-tabbar__item weui-bar__item--on">
+                <span class="weui-badge" style="position: absolute;top: -.4em;right: 1em;">8</span>
+                <div class="weui-tabbar__icon">
+                    <img src="./images/icon_nav_button.png" alt="">
+                </div>
+                <p class="weui-tabbar__label">微信</p>
+            </a>
+            <a href="javascript:;" class="weui-tabbar__item">
+                <div class="weui-tabbar__icon">
+                    <img src="./images/icon_nav_msg.png" alt="">
+                </div>
+                <p class="weui-tabbar__label">通讯录</p>
+            </a>
+            <a href="javascript:;" class="weui-tabbar__item">
+                <div class="weui-tabbar__icon">
+                    <img src="./images/icon_nav_article.png" alt="">
+                </div>
+                <p class="weui-tabbar__label">发现</p>
+            </a>
+            <a href="javascript:;" class="weui-tabbar__item">
+                <div class="weui-tabbar__icon">
+                    <img src="./images/icon_nav_cell.png" alt="">
+                </div>
+                <p class="weui-tabbar__label">我</p>
+            </a>
+        </div>
+    </div>
+    <div class="weui-tab">
+        <div class="weui-tab__bd">
+            <div id="tab1" class="weui-tab__bd-item weui-tab__bd-item--active">
+                <h1>页面一</h1>
+            </div>
+            <div id="tab2" class="weui-tab__bd-item">
+                <h1>页面二</h1>
+            </div>
+            ...
+        </div>
+
+        <div class="weui-tabbar">
+            <a href="#tab1" class="weui-tabbar__item weui-bar__item--on">
+                ...
+            </a>
+            <a href="#tab2" class="weui-tabbar__item">
+                ...
+            </a>
+            ...
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <form action="{{url('/shijuanInsert')}}" method="post" style="border: 1px solid darkgray;border-radius: 10px;height: 400px;width: 80%;margin-top: 100px">
         <div class="weui-cells__title" style="font-size: 22px;text-align: center;color: green">试题选择</div>
         <div class="weui-cells__title">选择章节</div>
