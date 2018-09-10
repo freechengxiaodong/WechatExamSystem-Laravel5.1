ad4f7c9486dae8c81a1992ae2c51e21f66970c67
freecheng <1678029425@qq.com>
2018-09-10 13:06:41 +0800

    cxd

--- resources/views/Index/chooseshijuan.blade.php
+++ resources/views/Index/chooseshijuan.blade.php
@@ -15,37 +15,42 @@
                 选项二
             </div>
         </div>
-    </div>
-        <form action="{{url('/shijuanInsert')}}" method="post" style="border: 1px solid darkgray;border-radius: 10px;height: 400px;width: 80%;margin-top: 100px">
-            <div class="weui-cells__title" style="font-size: 22px;text-align: center;color: green">试题选择</div>
-            <div class="weui-cells__title">选择章节</div>
-            <div class="weui-cell weui-cell_select">
-                <div class="weui-cell__bd">
-                    <select class="weui-select" name="zhangjie">
-                        <option selected value="第一章">第一章</option>
-                        <option value="第二章">第二章</option>
-                        <option value="第三章">第三章</option>
-                        <option value="第四章">第四章</option>
-                        <option value="第五章">第五章</option>
-                    </select>
+        <div class="weui-tab__bd">
+
+            <form action="{{url('/shijuanInsert')}}" method="post" style="border: 1px solid darkgray;border-radius: 10px;height: 400px;width: 80%;margin-top: 100px">
+                <div class="weui-cells__title" style="font-size: 22px;text-align: center;color: green">试题选择</div>
+                <div class="weui-cells__title">选择章节</div>
+                <div class="weui-cell weui-cell_select">
+                    <div class="weui-cell__bd">
+                        <select class="weui-select" name="zhangjie">
+                            <option selected value="第一章">第一章</option>
+                            <option value="第二章">第二章</option>
+                            <option value="第三章">第三章</option>
+                            <option value="第四章">第四章</option>
+                            <option value="第五章">第五章</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            <div class="weui-cells__title">选择题数</div>
-            <div class="weui-cell weui-cell_select">
-                <div class="weui-cell__bd">
-                    <select class="weui-select" name="count">
-                        <option selected value="5">5</option>
-                        <option value="10">10</option>
-                        <option value="15">15</option>
-                        <option value="20">20</option>
-                        <option value="30">30</option>
-                    </select>
+                <div class="weui-cells__title">选择题数</div>
+                <div class="weui-cell weui-cell_select">
+                    <div class="weui-cell__bd">
+                        <select class="weui-select" name="count">
+                            <option selected value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
-            {{csrf_field()}}
-            <input type="hidden" name="openid" value="">
-            <button class="weui-btn weui-btn_plain-primary" style="width: 80%;margin-top: 40px">生成试卷</button>
-        </form>
+                {{csrf_field()}}
+                <input type="hidden" name="openid" value="">
+                <button class="weui-btn weui-btn_plain-primary" style="width: 80%;margin-top: 40px">生成试卷</button>
+            </form>
+
+        </div>
+
+
 
 
 
