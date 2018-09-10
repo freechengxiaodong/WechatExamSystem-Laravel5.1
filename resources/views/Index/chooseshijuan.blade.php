75cd007a2335f539cf3b300ac5315056bffabed3
freecheng <1678029425@qq.com>
2018-09-10 12:48:18 +0800

    cxd

--- resources/views/Index/chooseshijuan.blade.php
+++ resources/views/Index/chooseshijuan.blade.php
@@ -23,35 +23,6 @@
             <input type="text" id="a">
 
         </div>
-
-        <div class="weui-tabbar">
-            <a href="javascript:;" class="weui-tabbar__item weui-bar__item--on">
-                <span class="weui-badge" style="position: absolute;top: -.4em;right: 1em;">8</span>
-                <div class="weui-tabbar__icon">
-                    <img src="./images/icon_nav_button.png" alt="">
-                </div>
-                <p class="weui-tabbar__label">微信</p>
-            </a>
-            <a href="javascript:;" class="weui-tabbar__item">
-                <div class="weui-tabbar__icon">
-                    <img src="./images/icon_nav_msg.png" alt="">
-                </div>
-                <p class="weui-tabbar__label">通讯录</p>
-            </a>
-            <a href="javascript:;" class="weui-tabbar__item">
-                <div class="weui-tabbar__icon">
-                    <img src="./images/icon_nav_article.png" alt="">
-                </div>
-                <p class="weui-tabbar__label">发现</p>
-            </a>
-            <a href="javascript:;" class="weui-tabbar__item">
-                <div class="weui-tabbar__icon">
-                    <img src="./images/icon_nav_cell.png" alt="">
-                </div>
-                <p class="weui-tabbar__label">我</p>
-            </a>
-        </div>
-    </div>
     <div class="weui-tab">
         <div class="weui-tab__bd">
             <div id="tab1" class="weui-tab__bd-item weui-tab__bd-item--active">
