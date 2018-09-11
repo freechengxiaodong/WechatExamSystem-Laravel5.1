8f515ffd448ac6f637bab124d821ee4ea9be158b
freecheng <1678029425@qq.com>
2018-09-11 19:08:30 +0800

    cxd

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -111,8 +111,11 @@ class IndexController extends Controller
 //        }
         $startdate=$res->created_at;
         $enddate=date("y-m-d H:i:s");
+        echo strtotime($enddate);die;
+        $date=floor((strtotime($enddate)-strtotime($startdate))/86400);
+        $hour=floor((strtotime($enddate)-strtotime($startdate))%86400/3600);
         $minute=floor((strtotime($enddate)-strtotime($startdate))%86400/60);
-        echo $minute;die;
+        echo $minute;
         if($minute>=1){
             $title = 'error';
             $content = '试卷已超过1分钟,过期!';
