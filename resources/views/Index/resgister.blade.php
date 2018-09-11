114c1fc4e3723929bbe52de618bb42bc446afb36
freecheng <1678029425@qq.com>
2018-09-11 18:15:17 +0800

    cxd

--- resources/views/Index/resgister.blade.php
+++ resources/views/Index/resgister.blade.php
@@ -8,13 +8,13 @@
     </style>
     <form action="{{url('/studentInfoInsert')}}" method="post" style="height: 300px;width: 80%;margin-top: 60px">
         <div class="weui-cells__title" style="font-size: 22px;text-align: center;color: green;">学生信息绑定</div>
-        <div class="weui-cell" style="border: 1px solid grey;border-radius: 3px;margin-top: 100px;">
+        <div class="weui-cell" style="border: 1px solid grey;border-radius: 3px;margin-top: 50px;">
             <div class="weui-cell__hd"><label class="weui-label" style="width: 60px">姓名</label></div>
             <div class="weui-cell__bd">
                 <input class="weui-input" type="text" name="name" placeholder="请输入姓名">
             </div>
         </div>
-        <div class="weui-cell" style="border: 1px solid grey;border-radius: 3px;margin-top: 5px">
+        <div class="weui-cell" style="border: 1px solid grey;border-radius: 3px;margin-top: 10px">
             <div class="weui-cell__hd"><label class="weui-label" style="width: 60px">学号</label></div>
             <div class="weui-cell__bd">
                 <input class="weui-input" type="number" name="number" pattern="[0-9]*" placeholder="请输学号">
