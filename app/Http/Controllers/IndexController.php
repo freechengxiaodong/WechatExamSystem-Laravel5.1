6d944aa52f225d22913f2845dc2642c127fff832
freecheng <1678029425@qq.com>
2018-09-11 18:31:49 +0800

    cxd

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -32,7 +32,12 @@ class IndexController extends Controller
         $usr = new Student();
         $usrinfo = $usr->where('openid','=',"$openid")->first();
         if($usrinfo){
-            //已注册过,直接考试
+            //已注册过,判断用户是学生还是老师
+            $res = DB::table('teachers')->where('openid','=',"$openid")->first();
+            if($res){
+                //进入老师页
+                echo "<script>location.href='".'/createshijuan'."';</script>";
+            }
             //上一页$_SERVER["HTTP_REFERER"]
             echo "<script>location.href='".'/shijuan'."';</script>";
         }else{
