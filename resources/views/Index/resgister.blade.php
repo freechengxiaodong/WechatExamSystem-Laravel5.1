168c0ba11659d3afafd08408724178da3eb0f50a
freecheng <1678029425@qq.com>
2018-09-11 18:13:22 +0800

    cxd

--- resources/views/Index/resgister.blade.php
+++ resources/views/Index/resgister.blade.php
@@ -7,14 +7,14 @@
         }
     </style>
     <form action="{{url('/studentInfoInsert')}}" method="post" style="height: 300px;width: 80%;margin-top: 60px">
-        <div class="weui-cells__title" style="font-size: 22px;text-align: center;color: green;margin-top: 80px">学生信息绑定</div>
-        <div class="weui-cell" style="border: 1px solid black">
+        <div class="weui-cells__title" style="font-size: 22px;text-align: center;color: green;margin-top: 100px">学生信息绑定</div>
+        <div class="weui-cell" style="border: 1px solid grey;border-radius: 3px;">
             <div class="weui-cell__hd"><label class="weui-label" style="width: 60px">姓名</label></div>
             <div class="weui-cell__bd">
                 <input class="weui-input" type="text" name="name" placeholder="请输入姓名">
             </div>
         </div>
-        <div class="weui-cell" style="border: 1px solid black;margin-top: 5px">
+        <div class="weui-cell" style="border: 1px solid grey;border-radius: 3px;margin-top: 5px">
             <div class="weui-cell__hd"><label class="weui-label" style="width: 60px">学号</label></div>
             <div class="weui-cell__bd">
                 <input class="weui-input" type="number" name="number" pattern="[0-9]*" placeholder="请输学号">
@@ -22,7 +22,7 @@
         </div>
         {{csrf_field()}}
         <input type="hidden" name="openid" value="{{$openid}}">
-        <button class="weui-btn weui-btn_plain-primary" style="width: 100%;margin-top: 40px">确认提交</button>
+        <button class="weui-btn weui-btn_plain-primary" style="width: 100%;margin-top: 80px">确认提交</button>
     </form>
 
     @endsection
